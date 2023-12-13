a32a50a778fdcf50c31dfaed2bcb1d9751c5df48
echia1992 <echiaanthony1@gmail.com>
2023-12-13 22:50:57 +0100

    AE: creating team section ,contact page and hero section new update on all the section and the navbar

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
-import './globals.css'
+import '@/styles/main.scss'
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,8 +11,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+   <html lang="en">
+  <body className={inter.className}>
+  <Navbar />
+  <div className='mt-5'>
+    {children}
+  </div>
+  </body>
+   </html>
   )
 }
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 const Hero =()=> {
 
         return (
-            <div>
-                <section className="py-28">
+            <div className='mt-0'>
+                <section className="py-8">
                     <div
                         className="max-w-screen-xl mx-auto text-gray-600 gap-x-12 items-center justify-between overflow-hidden md:flex md:px-8">
                         <div className="flex-none space-y-5 px-4 sm:max-w-lg md:px-0 lg:max-w-xl">
