019529024d13f2d2632d6fa8feb7c2d06f94624c
echia1992 <echiaanthony1@gmail.com>
2023-12-14 12:59:39 +0100

    AE: mobile responsive

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Register() {
     return (
-        <div className='h-screen flex justify-center items-center space-x-3 space-y-2'>
-            <h1 className='text-4xl text-center'> Coming Soon</h1>
+        <div className='h-screen sm:flex justify-center items-center space-x-3 space-y-2 '>
+            <h1 className='text-2xl sm:text-4xl text-center '> Coming Soon</h1>
             <p className='mt-16'>Please use the contact us page and send me to us Thank you. </p>
             <Link className='text-green-600' href='/contact'>Contact Us</Link>
         </div>
